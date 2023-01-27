e6d6c979db34d615f58e5cafb3bde77483172ffa
Aidan Neal <decator.c@proton.me>
2023-01-27 07:39:17 -0500

    Add register fn

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,50 @@
+use serenity::{builder::CreateApplicationCommand, model::prelude::command::CommandOptionType};
+
 /// Temperature conversions
-pub mod temp;
+pub mod temperature;
 
 /// Conversions between 12 and 24 hour time
 pub mod time;
+
+pub fn register(command: &mut CreateApplicationCommand) -> &mut CreateApplicationCommand
+{
+    command
+        .name("conversions")
+        .description("Convert between values")
+        .dm_permission(true)
+        .create_option(|option| {
+            option
+                .name("temperature")
+                .kind(CommandOptionType::SubCommand)
+                .description("Convert from one temperature unit to another. Supports Kelvin, Fahrenheit, and Celcius.")
+                .create_sub_option(|option| {
+                    option
+                        .name("value")
+                        .description("Original value (e.g. '65F' [Fahrenheit], '18.33C' [Celsius].")
+                        .kind(CommandOptionType::String)
+                        .required(true)
+                })
+                .create_sub_option(|option| {
+                    option
+                        .name("target")
+                        .description("The unit to target. (e.g 'F' [Fahrenheit], 'K' [kelvin]).")
+                        .kind(CommandOptionType::String)
+                        .required(true)
+                })
+        })
+        .create_option(|option| {
+            option
+                .name("Hours")
+                .kind(CommandOptionType::SubCommand)
+                .description("Some people don't know how to subtract by '12'.")
+                .create_sub_option(|option| {
+                    option
+                        .name("time")
+                        .description(
+                            "Time in 24h time ('6:00', '14:30'), or in 12h time ('4:44am', '6:00pm')",
+                        )
+                        .kind(CommandOptionType::String)
+                        .required(true)
+                })
+        })
+}
