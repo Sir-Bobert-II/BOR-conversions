2e822f35b78ec729d94f14eff4d1f7542378290d
Aidan Neal <decator.c@proton.me>
2023-02-08 14:43:40 -0500

    Define help message

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use lazy_static::lazy_static;
 use serenity::{builder::CreateApplicationCommand, model::prelude::command::CommandOptionType};
 
 /// Temperature conversions
@@ -84,4 +85,69 @@ pub fn register(command: &mut CreateApplicationCommand) -> &mut CreateApplicatio
                         .required(true)
                 })
         })
+}
+
+lazy_static! {
+    pub static ref HELP: String = {
+        help::HelpMessage::new()
+            .name("conversions")
+            .description("Convert between values")
+            .add_subcommand(
+                help::HelpMessage::new()
+                    .name("temperature")
+                    .description("Convert from one temperature unit to another. Supports Kelvin, Fahrenheit, and Celcius")
+                    .add_option({
+                        help::HelpMessageOption::new()
+                            .name("value")
+                            .kind("String")
+                            .description("Original value (e.g. '65F' [Fahrenheit], '18.33C' [Celsius])")
+                            .required(true)
+                            .clone()
+                    })
+                    .add_option({
+                        help::HelpMessageOption::new()
+                            .name("target")
+                            .kind("String")
+                            .description("The unit to target. (e.g 'F' [Fahrenheit], 'K' [kelvin])")
+                            .required(true)
+                            .clone()
+                    })
+                    .clone(),
+            )
+            .add_subcommand(help::HelpMessage::new()
+                    .name("currency")
+                    .description("Convert from one currency to another.")
+                    .add_option({
+                        help::HelpMessageOption::new()
+                            .name("input")
+                            .kind("String")
+                            .description("The input currency (e.g. '$74', '80.90 CAD', '20 quid')")
+                            .required(true)
+                            .clone()
+                    })
+                    .add_option({
+                        help::HelpMessageOption::new()
+                            .name("target")
+                            .kind("String")
+                            .description("The currency to convert to (e.g 'rubles', 'usd', 'yen'). Supported currencies: USD, EUR, CAD, RUB, JPY, AUD, AMD, and GBP")
+                            .required(true)
+                            .clone()
+                    })
+                    .clone()
+                )
+            .add_subcommand(
+                help::HelpMessage::new()
+                .name("hours")
+                .description("Some people don't know how to subtract by '12'")
+                .add_option(
+                    help::HelpMessageOption::new()
+                    .name("time")
+                    .description("Time in 24h time ('6:00', '14:30'), or in 12h time ('4:44am', '6:00pm')")
+                    .required(true)
+                    .kind("String")
+                    .clone()
+                ).clone()
+            )
+            .to_string()
+    };
 }
